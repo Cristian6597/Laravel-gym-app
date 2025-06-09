ae3be9dd8a06057c92d1c056fa05899bb924a547
Cristian6597 <cristianercolani6597@gmail.com>
2025-06-09 15:30:52 +0200

    fixate tabelle

--- database/migrations/2025_05_14_134952_create_trainers_table.php
+++ database/migrations/2025_05_14_134952_create_trainers_table.php
@@ -13,10 +13,7 @@ return new class extends Migration
     {
         Schema::create('trainers', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->string('first_name');
             $table->id();
-            $table->string('email')->unique();
-            $table->string('last_name');
             $table->string('phone')->nullable();
             $table->date('birth_date')->nullable();
             $table->string('specialty')->nullable(); // es: cardio, pesi, yoga
